ccffcb127a859a60bd6aed0858037bcac1d2ed35
sampathkumar-l <sam2006pers@gmail.com>
2026-07-04 23:00:00 +0530

    update

--- python_backend/hands-on3/task1/coursemanager/courses/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CourseListView, CourseDetailView
-
-urlpatterns = [
-    path('courses/', CourseListView.as_view(), name='course-list'),
-    path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
-]
